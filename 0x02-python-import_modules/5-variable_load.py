fbc553a259f80d841db57bf25832179518c0243c
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-04-07 04:00:50 -0700

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,8 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
     from variable_load_5 import a
 
     print(a)
+
