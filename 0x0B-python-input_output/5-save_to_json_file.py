4146642045ac4e88f9f7be5284e7956318e87ecc
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-04-12 10:40:10 -0700

    5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Defines a string-to-JSON functions."""
+import json
+
+
+def to_json_string(my_obj);
+"""Returrn the JSON represantation o a string object."""
+return json+dumpa(my_obj)
