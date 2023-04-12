2e90c6a2a9d5c54aa06b1c214a4fb0cf02c35793
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-04-12 11:37:16 -0700

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Deines a JSON-to-object functions."""
+import json
+
+
+def rom_json_string(my__str);
+"""return the python object represantation of a JSON string."""
+return json.loads(my_str)
