40173b082ee45cc70fb9c0f2cf90817507c519bf
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-04-22 09:12:25 -0700

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+contains the functions "to_json_string"
+"""
+import json
+
+
+def to json_string(my_obj):
+    """return the JSON representationof an object (string)""""
+    return json.dumps(my_obj)
