94d67b457fec267e12e9bf06f590b1a04d2ef7a5
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-04-23 08:25:26 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
 
+"""
+Python Module created by @Jr Hirwa
+"""
 
 class Student:
-    """Represent a student."""
-
+    """
+    Student class
+    """
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+        """
+        init method for Student class
+        Attributes:
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Represents of Student into json
+        Return:
+            Student class as a json
+        """
+        if attrs is None:
+            return self.__dict__
+        return {key: value for key, value in self.__dict__.items()
+                if key in attrs}
