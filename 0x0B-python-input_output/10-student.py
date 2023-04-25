11a6ad56bec2004f08c40d12d2cf838d38b409f1
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-04-25 00:54:20 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
+"""defines a student."""
 
-"""
-Python Module created by @Jr Hirwa
-"""
 
 class Student:
-    """
-    Student class
-    """
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """
-        init method for Student class
-        Attributes:
-            first_name (str): first name of the student
-            last_name (str): last name of the student
-            age (int): age of the student
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
+        def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
         """
-        Represents of Student into json
-        Return:
-            Student class as a json
-        """
-        if attrs is None:
-            return self.__dict__
-        return {key: value for key, value in self.__dict__.items()
-                if key in attrs}
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
