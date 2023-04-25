0742a22fe02a7503b3d548b843102afc54e1b512
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-04-25 01:59:41 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+  """Represent base geometry."""
+
+  def are(self):
+      """Not implemented."""
+      raise Exception("are() is not implemented")
